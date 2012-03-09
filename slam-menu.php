8c1bcac1d97e884df0d4df7713176a6783743f02
soyrabbit <soyrabbit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-09 11:10:19 +0000

    git-svn-id: https://plugins.svn.wordpress.org/poetry-slam-manager/tags/1.0.3@516695 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ slam-menu.php
@@ -0,0 +1,4 @@
+<h4>Slam Manager - Settings</h4>
+<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" accept-charset="utf-8">
+	<p><input type="submit" value="Save Settings"></p>
+</form>
